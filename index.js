726ad64951590664d88dc251db0bb454f9673885
Matt Price <matt.price@utoronto.ca>
2019-10-17 07:47:19 -0400

    index: add index.js script tag & starter code

---
+++ index.js
@@ -0,0 +1,7 @@
+//add your code here
+function runOnLoad () {
+
+}
+
+// "listen" for the "DOMContentLoaded" event, and then run "runOnLoad()"
+document.addEventListener('DOMContentLoaded', runOnLoad, false);
