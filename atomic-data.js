d3a2eb1f4930ae597fc773decfb122ba6a3ebed5
Matt Price <matt.price@utoronto.ca>
2020-10-29 13:20:20 -0400

    oops, also add atomic functions data :-)

---
+++ atomic-data.js
@@ -0,0 +1,64 @@
+const allEntries = [
+  { name: 'Nikita', blurb: 'Drunken recitations of Russian poetry' },
+  { name: 'Catherine', blurb: 'being ambidextrous' },
+  { name: 'Clyde', blurb: 'Juggling' },
+  {
+    name: 'Drew',
+    blurb: 'Drinking only one cup of coffee and having the effects last all day.'
+  },
+  { name: 'Jade', blurb: 'Memorizing and singing patter songs' },
+  { name: 'Kaan', blurb: 'photography' },
+  { name: 'Matt', blurb: 'Stress' },
+  { name: 'Natalie', blurb: 'having the best Shakira impersonation' },
+  { name: 'The Big Gay', blurb: 'Topping, Crying in Robarts' },
+  { name: 'Zhaoyang', blurb: 'Improvised Solutions' },
+  { name: 'Akeil', blurb: 'anxiety.' },
+  { name: 'ArchAndroid', blurb: 'time travel, power of love' },
+  {
+    name: 'But you can call me Brianna',
+    blurb: 'I make a mean cup of tea n coffee'
+  },
+  {
+    name: 'Caitlin',
+    blurb: 'Managing to keep a four year old store-bought orchid alive, but not alive enough to bloom'
+  },
+  {
+    name: 'dannaste',
+    blurb: 'interdisciplinary landscape poetics to stay human'
+  },
+  {
+    name: 'Donielle',
+    blurb: 'Completing tasks using the slowest, most complicated methods possible.'
+  },
+  { name: 'Wayne', blurb: 'eating alot of pasta' },
+  {
+    name: 'Grace',
+    blurb: 'Wearing shorts until the temperature is in single digits'
+  },
+  {
+    name: 'Grace',
+    blurb: 'Reviving dying plants/raising food from soil'
+  },
+  { name: 'Gagan', blurb: 'late night food munching' },
+  { name: 'Allie', blurb: 'oversleeping' },
+  { name: 'Avatar', blurb: 'Master of All Four Elements' },
+  { name: 'Nathan', blurb: 'Proscratination, Eating alot' },
+  {
+    name: 'Matt',
+    blurb: 'cooking, I can cook good food quick and easily'
+  },
+  { name: 'Naruto', blurb: 'jinchūriki, ninjutsu' },
+  {
+    name: 'Neeharika',
+    blurb: 'knitting really fast when I am stressed'
+  },
+  { name: 'Wolverine', blurb: 'Pianist' },
+  { name: 'Sky', blurb: 'adding and dropping programs' },
+  { name: 'Spidey', blurb: 'spidey-sense, sticky hands, venom sting' },
+  { name: 'Keener', blurb: 'late night cramming' },
+  { name: 'Bones', blurb: 'falling off cliffs, jumping off bridges' },
+  { name: 'Adskfjoisafpdldfl', blurb: 'staying alive' },
+  { name: 'JOKER', blurb: 'Psychic' },
+  { name: 'BumbleZ', blurb: 'remarkable ability to be distracted' }
+],
+      sample = {name: 'matt', blurb: 'just some guy'};
