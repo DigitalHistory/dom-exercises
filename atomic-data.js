680d1133e25d0b121a28e332474f3b9c5e277e72
Matt Price <matt.price@utoronto.ca>
2021-10-19 14:00:37 -0400

    update for 2021

--- atomic-data.js
+++ atomic-data.js
@@ -1,63 +1,63 @@
 const allEntries = [
-  { name: 'Nikita', blurb: 'Drunken recitations of Russian poetry' },
-  { name: 'Catherine', blurb: 'being ambidextrous' },
-  { name: 'Clyde', blurb: 'Juggling' },
+  { name: 'someone', blurb: 'Drunken recitations of Russian poetry' },
+  { name: 'someone', blurb: 'being ambidextrous' },
+  { name: 'someone', blurb: 'Juggling' },
   {
-    name: 'Drew',
+    name: 'someone',
     blurb: 'Drinking only one cup of coffee and having the effects last all day.'
   },
-  { name: 'Jade', blurb: 'Memorizing and singing patter songs' },
-  { name: 'Kaan', blurb: 'photography' },
-  { name: 'Matt', blurb: 'Stress' },
-  { name: 'Natalie', blurb: 'having the best Shakira impersonation' },
-  { name: 'The Big Gay', blurb: 'Topping, Crying in Robarts' },
-  { name: 'Zhaoyang', blurb: 'Improvised Solutions' },
-  { name: 'Akeil', blurb: 'anxiety.' },
-  { name: 'ArchAndroid', blurb: 'time travel, power of love' },
+  { name: 'someone', blurb: 'Memorizing and singing patter songs' },
+  { name: 'someone', blurb: 'photography' },
+  { name: 'someone', blurb: 'Stress' },
+  { name: 'someone', blurb: 'having the best Shakira impersonation' },
+  { name: 'someone', blurb: 'Topping, Crying in Robarts' },
+  { name: 'someone', blurb: 'Improvised Solutions' },
+  { name: 'someone', blurb: 'anxiety.' },
+  { name: 'someone', blurb: 'time travel, power of love' },
   {
-    name: 'But you can call me Brianna',
+    name: "someone",
     blurb: 'I make a mean cup of tea n coffee'
   },
   {
-    name: 'Caitlin',
+    name: 'someone',
     blurb: 'Managing to keep a four year old store-bought orchid alive, but not alive enough to bloom'
   },
   {
-    name: 'dannaste',
+    name: 'someone',
     blurb: 'interdisciplinary landscape poetics to stay human'
   },
   {
-    name: 'Donielle',
+    name: 'someone',
     blurb: 'Completing tasks using the slowest, most complicated methods possible.'
   },
-  { name: 'Wayne', blurb: 'eating alot of pasta' },
+  { name: 'someone', blurb: 'eating alot of pasta' },
   {
-    name: 'Grace',
+    name: 'someone',
     blurb: 'Wearing shorts until the temperature is in single digits'
   },
   {
-    name: 'Grace',
+    name: 'someone',
     blurb: 'Reviving dying plants/raising food from soil'
   },
-  { name: 'Gagan', blurb: 'late night food munching' },
-  { name: 'Allie', blurb: 'oversleeping' },
+  { name: 'someone', blurb: 'late night food munching' },
+  { name: 'someone', blurb: 'oversleeping' },
   { name: 'Avatar', blurb: 'Master of All Four Elements' },
-  { name: 'Nathan', blurb: 'Proscratination, Eating alot' },
+  { name: 'someone', blurb: 'Proscratination, Eating alot' },
   {
     name: 'Matt',
     blurb: 'cooking, I can cook good food quick and easily'
   },
   { name: 'Naruto', blurb: 'jinchūriki, ninjutsu' },
   {
-    name: 'Neeharika',
+    name: 'someone',
     blurb: 'knitting really fast when I am stressed'
   },
   { name: 'Wolverine', blurb: 'Pianist' },
-  { name: 'Sky', blurb: 'adding and dropping programs' },
+  { name: 'someone', blurb: 'adding and dropping programs' },
   { name: 'Spidey', blurb: 'spidey-sense, sticky hands, venom sting' },
   { name: 'Keener', blurb: 'late night cramming' },
   { name: 'Bones', blurb: 'falling off cliffs, jumping off bridges' },
-  { name: 'Adskfjoisafpdldfl', blurb: 'staying alive' },
+  { name: 'someone', blurb: 'staying alive' },
   { name: 'JOKER', blurb: 'Psychic' },
   { name: 'BumbleZ', blurb: 'remarkable ability to be distracted' }
 ],
